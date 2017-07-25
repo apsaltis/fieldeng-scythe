709b75b2ac67183b8ee60256d601b2e232cc0453
khaslbeck <khaslbeck@hw12629.home>
2017-07-25 09:47:12 -0400

    cronus helper returning a map with all signals

--- src/main/scala/com/hortonworks/scythe/cronus/Helper.scala
+++ src/main/scala/com/hortonworks/scythe/cronus/Helper.scala
@@ -2,56 +2,61 @@ package com.hortonworks.scythe.cronus
 
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
-import java.sql.{Date, Timestamp}
+import java.sql.{ Date, Timestamp }
 import com.hortonworks.scythe.LinearInterpolation
 import java.text.SimpleDateFormat
 
-
 class Helper {
-  
-  
-  def show[T](ds:org.apache.spark.sql.Dataset[T]): Unit = {
+
+  def show[T](ds: org.apache.spark.sql.Dataset[T]): Unit = {
     ds.show()
   }
-  
-  def interpolate[T](s1: String, s2: List[String], df: org.apache.spark.sql.DataFrame )  {
-    interpolate(s1, s2, "function", df)      
+
+  def interpolate[T](s1: String, s2: List[String], df: org.apache.spark.sql.DataFrame) {
+    interpolate(s1, s2, "function", df)
   }
-  
+
   /*def interpolate[T](s1: String, s2: List[String], ds: org.apache.spark.sql.Dataset[T] )  {
     interpolate(s1, s2, "function", ds.toDF())      
   }*/
-  
-  def interpolate(s1: String, s2: List[String], colSigName: String, df: org.apache.spark.sql.DataFrame ) : List[Tuple2[java.util.Date, Double]] =  {
-    
-    val ds1= df.filter(df.col(colSigName) === s1)
-    //val ds1 = df.filter(s" '${colSigName}' == '${s1}'")
-    
+
+  def interpolate(s1: String, s2: List[String], colSigName: String, df: org.apache.spark.sql.DataFrame): Map[String, List[(String, java.util.Date, Double)]] = {
+
+    var map = Map[String, List[(String, java.util.Date, Double)]]()
+
+    val ds1 = df.filter(df.col(colSigName) === s1)
     val s = ds1.collect()
-    
+
     // tagName, timestamp, value
-    val rs = s.map { x=> ( 
-        x(0).toString(), 
-        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(x(1).toString()), 
-        x(2).asInstanceOf[Double]) }.toList
-    
-    
-    var as = List[(java.util.Date, Double)]()
+    val rs = s.map { x =>
+      ( x(0).toString(),
+        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(x(1).toString()),
+        x(2).asInstanceOf[Double])
+    }.toList
+
+    map = map + (s1 -> rs)
+
     for (sigName <- s2) {
-      val ds2 = df.filter(s"${colSigName} == '${sigName}'") 
-  
-        val rs2 = ds2.collect.map { x=> ( 
-        x(0).toString(), 
-        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(x(1).toString()), 
-        x(2).asInstanceOf[Double]) }.toList
-            
-      val l = new LinearInterpolation
-      as = l.interpolateDate(
-          rs.map {case (tag,time,value) => (time, value)}, 
-          rs2.map{case (tag,time,value) => (time, value)}
-          )
+
+      if (sigName != s1) { // do not interpolate to the base signal
+        val ds2 = df.filter(s"${colSigName} == '${sigName}'")
+
+        val rs2 = ds2.collect.map { x =>
+          (
+            x(0).toString(),
+            new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(x(1).toString()),
+            x(2).asInstanceOf[Double])
+        }.toList
+
+        val l = new LinearInterpolation
+        val interp = l.interpolateDate(
+          rs.map { case (tag, time, value) => (time, value) },
+          rs2.map { case (tag, time, value) => (time, value) })
+
+        map = map + (sigName -> (rs2.map(x => x._1) zip interp).map(x => (x._1, x._2._1, x._2._2)))
+      }
     }
-    return as
+    return map
   }
-  
+
 }
---
+++ src/test/scala/com/hortonworks/scythe/cronus/Helper.scala
@@ -0,0 +1,57 @@
+package com.hortonworks.scythe.cronus
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.scalatest.junit.AssertionsForJUnit
+
+import org.apache.spark.sql._
+import java.text.SimpleDateFormat
+import org.apache.spark
+//import sqlContext.implicits._
+import org.apache.spark.sql.SparkSession
+
+class HelperTest {
+
+  case class Signal(tagNm: String, date: java.sql.Timestamp, value: Double)
+
+  @Before def initialize() {
+    println("Dataset Tests")
+  }
+
+  @Test def interpolate() {
+    val ch = new com.hortonworks.scythe.cronus.Helper
+
+    val spark = SparkSession
+      .builder()
+      .master("local")
+      .appName("Scythe Test Cases")
+      .getOrCreate()
+
+    import spark.implicits._
+    import spark.sqlContext._
+
+    val df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+
+    val ds = Seq(
+      ("mpg1", "2017-04-01T11:05:27.000Z", 22.0),
+      ("mpg1", "2017-04-01T11:05:28.000Z", 32.0),
+      ("mpg1", "2017-04-01T11:05:29.000Z", 42.0),
+
+      ("mpg2", "2017-04-01T11:05:27.000Z", 23.0),
+      ("mpg2", "2017-04-01T11:05:29.000Z", 43.0)).toDF("function", "time", "value")
+
+    ds.show()
+    print(ds)
+
+    val map = ch.interpolate("mpg1", List("mpg2"), "function", ds)
+
+    println(map)
+
+    val mpg1 = map.get("mpg1")
+    val mpg2 = map.get("mpg2")
+    Assert.assertEquals(22.0, mpg1.get(0)._3, 0.001)
+    Assert.assertEquals(33.0, mpg2.get(1)._3, 0.001)
+  }
+
+}
